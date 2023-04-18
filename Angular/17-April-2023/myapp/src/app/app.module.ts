47245e5429f4b11a9d54f766f54f3c79e33dc5f7
KBPheko <@J8riW2aG*>
2023-04-18 15:44:56 +0200

    created create_product component

--- Angular/17-April-2023/myapp/src/app/app.module.ts
+++ Angular/17-April-2023/myapp/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { HttpClientModule} from '@angular/common/http'
+import { HttpClientModule} from '@angular/common/http';
+import { CreateProductComponent } from './components/create-product/create-product.component'
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CreateProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Angular/17-April-2023/myapp/src/app/components/create-product/create-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-product',
+  templateUrl: './create-product.component.html',
+  styleUrls: ['./create-product.component.css']
+})
+export class CreateProductComponent {
+
+  name = new FormControl('')
+}
