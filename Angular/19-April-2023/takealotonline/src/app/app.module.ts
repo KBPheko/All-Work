65ba07978031726341733a830054be8243f20673
KBPheko <@J8riW2aG*>
2023-04-20 16:06:26 +0200

    Created a Takealot look-alike website

---
+++ Angular/19-April-2023/takealotonline/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductComponent } from './pages/product/product.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { ProductCartComponent } from './components/product-cart/product-cart.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    SliderComponent,
+    ProductCartComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
